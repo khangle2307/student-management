f7b4f64c6647b482a9ab946fbb2963815a58a9dc
khangle23 <khanglmph14697@fpt.edu.vn>
2022-08-22 12:51:26 +0700

    publish release v1.1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableComponent } from './components/table/table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'student',
+    pathMatch: 'full'
+  },
+  {
+    path: 'student',
+    component: TableComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
