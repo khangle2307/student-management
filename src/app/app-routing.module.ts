eeeb2dd5065d7d46bb843a1b7b6b5c13d5699b59
khangle23 <khanglmph14697@fpt.edu.vn>
2022-08-22 14:33:09 +0700

    publish release v1.2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { TableComponent } from './components/table/table.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'list',
-    pathMatch: 'full'
-  },
-  {
-    path: 'list',
-    component: TableComponent
-  }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http'
-import { TableComponent } from './components/table/table.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    TableComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
