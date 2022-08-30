84ef602546ef123e50be24fdb2b6374fa2cff762
khangle23 <khanglmph14697@fpt.edu.vn>
2022-08-30 14:49:00 +0700

    asdasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { TableComponent } from './components/table/table.component';
-import { ProductEditComponent } from './pages/product-edit/product-edit.component';
-import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    TableComponent,
-    ProductEditComponent,
-    PaginationComponent
   ],
   imports: [
     BrowserModule,
